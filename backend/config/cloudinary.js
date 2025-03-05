3f9b02897fd4d5e1ae3067207ca982ea3e28f9d4
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2025-03-05 16:48:46 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,18 +1,13 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary'
 
-const connectCloudinary = () => {
-    if (!process.env.CLOUDINARY_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_SECRET_KEY) {
-        console.error("❌ Cloudinary config missing. Please check environment variables.");
-        return;
-    }
-
+const connectCloudinary = async () => {
     cloudinary.config({
         cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_SECRET_KEY
-    });
+    })
 
-    console.log("✅ Cloudinary connected successfully");
-};
 
-export default connectCloudinary;
+}
+
+export default connectCloudinary
